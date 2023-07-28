e01d6641ba65c18886111a0f5d1df1b5c2899846
그램\wjdal <wjdals8625@naver.com>
2023-07-28 10:49:15 +0900

    vmfhwprxm todtjd

---
+++ src/main/java/com/spring/summerboot/SummerBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.summerboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/summerboot/SummerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.summerboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
